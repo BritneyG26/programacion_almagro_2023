8406a085bbc68b7edf356299c499fadf636809d8
ginna <ginna@LAPTOP-NM00FQ51>
2023-03-15 09:59:41 +0100

    Cambio de prueba de commit

--- Primer proyecto/src/ejercicio/maniqui/Boton.java
+++ Primer proyecto/src/ejercicio/maniqui/Boton.java
@@ -1,5 +1,47 @@
 package ejercicio.maniqui;
 
 public class Boton {
+	private String color;
+	private String forma;
+	private int tamanio;
+	
+	public Boton(String color, String forma) {
+		super();
+		this.color = color;
+		this.forma = forma;
+		
+	}
+
+	public Boton(String color, String forma, int tamanio) {
+		super();
+		this.color = color;
+		this.forma = forma;
+		this.tamanio = tamanio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getForma() {
+		return forma;
+	}
+
+	public void setForma(String forma) {
+		this.forma = forma;
+	}
+
+	public int getTamaniio() {
+		return tamanio;
+	}
+
+	public void setTamaniio(int tamaniio) {
+		this.tamanio = tamaniio;
+	}
+	
 
 }
